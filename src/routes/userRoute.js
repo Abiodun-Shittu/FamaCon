29ab4021f8e09cc939207233dc69752b8a01ede8
Abiodun Shittu <jerrywizklay1@gmail.com>
2023-04-22 22:18:45 +0100

    add the delete route

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {
 	createUser,
+	deleteUser,
 	getUser,
 	loginUser,
 	updateUser,
@@ -20,5 +21,6 @@ router.post(
 router.post("/login", validateRequiredParams(["email", "password"]), loginUser);
 router.get("/:userId", verifyToken, getUser);
 router.put("/:userId", verifyToken, updateUser);
+router.delete("/:userId", verifyToken, deleteUser);
 
 export default router;
